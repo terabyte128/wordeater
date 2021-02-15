5192ac1a5ea0b05326d88e0d9de134112d55ba9d
Sam Wolfson <wolfson@cs.uw.edu>
2021-02-14 17:53:48 -0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,89 @@
+import re
+import sqlite3
+
+from flask import request, Flask, g, render_template, flash, redirect, url_for
+
+app = Flask(__name__)
+app.secret_key = "somethingreallysecret"
+
+DATABASE = "db.sqlite3"
+
+
+def get_db() -> sqlite3.Connection:
+    db = getattr(g, "_database", None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, "_database", None)
+    if db is not None:
+        db.close()
+
+
+@app.before_first_request
+def create_tables():
+    db = get_db()
+    c = db.cursor()
+    c.execute(
+        """
+            CREATE TABLE IF NOT EXISTS words (
+            id integer primary key,
+            word text unique
+            )
+        """
+    )
+    db.commit()
+
+
+@app.route("/word", methods=("POST",))
+def add_word():
+    db = get_db()
+    c = db.cursor()
+
+    word = request.form["word"].lower().strip()
+    if not re.fullmatch(r"[a-z\s]+", word):
+        flash("Your word can only contain letters or spaces. Try again?")
+        return redirect(url_for("index"))
+
+    try:
+        c.execute("INSERT INTO words (word) VALUES (?)", (word,))
+        db.commit()
+    except sqlite3.IntegrityError:
+        pass
+
+    flash("Your word has been submitted! Why not submit another?")
+    return redirect(url_for("index"))
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/supersecretdontlookhere")
+def get_words():
+    db = get_db()
+    c = db.cursor()
+
+    c.execute("select word from words")
+    words = map(lambda x: x[0], c.fetchall())
+
+    return (",".join(words), 200)
+
+
+@app.route("/nukeit", methods=("DELETE",))
+def delete_words():
+    if request.form.get("really", "") != "yes":
+        return ("not allowed", 401)
+
+    db = get_db()
+    c = db.cursor()
+
+    c.execute("delete from words")
+
+    db.commit()
+
+    return ("", 204)
